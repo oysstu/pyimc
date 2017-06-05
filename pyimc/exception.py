4ce5e42dcd79e7b185cd332ef53237400ef03ab8
oysstu <oysstu@gmail.com>
2017-06-05 13:18:36 +0200

    Subclass KeyError instead of LookupError

--- pyimc/exception.py
+++ pyimc/exception.py
@@ -1,6 +1,6 @@
 
 
-class AmbiguousKeyError(LookupError):
+class AmbiguousKeyError(KeyError):
     def __init__(self, message, choices=None):
         super().__init__(message)
         self.choices = choices
