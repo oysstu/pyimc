c4eb567f9adcc111167e0895b18f6710b27d6dc2
oysstu <oysstu@gmail.com>
2017-07-28 22:52:59 +0200

    Working MessageList<T> implementation added

--- src/pbMessageList.hpp
+++ src/pbMessageList.hpp
@@ -1,36 +1,92 @@
+#include <string>
+
 #include <pybind11/pybind11.h>
 #include <pybind11/operators.h>
 
+#include <DUNE/IMC/Message.hpp>
 #include <DUNE/IMC/MessageList.hpp>
 
 namespace py = pybind11;
 using namespace DUNE::IMC;
 
-// TODO: MessageList is currently untested
+// Get typename for template parameter
+#if defined(__GNUG__) || defined(__clang__)
+#include <cstdlib>
+#include <cxxabi.h>
+template<typename T>
+std::string type_name() {
+    int status;
+    std::string tname = typeid(T).name();
+    char *demangled_name = abi::__cxa_demangle(tname.c_str(), NULL, NULL, &status);
+    if(status == 0) {
+        tname = demangled_name;
+        std::free(demangled_name);
+    }
+    // Erase namespace
+    tname.erase(0, strlen("DUNE::IMC::"));
+    return tname;
+}
+#else
+template<typename T>
+std::string type_name() {
+    return std::string(typeid(T).name());
+}
+#endif
+
+
+/*
+Note on object lifetimes:
+
+keep_alive<Nurse, Patient> indicates that the argument with index Patient should be kept alive 
+at least until the argument with index Nurse is freed by the garbage collector. Argument indices 
+start at one, while zero refers to the return value. For methods, index 1 refers to the implicit 
+this pointer, while regular arguments begin at index 2.
+
+py::keep_alive<1, 2>() - Object life of the first argument (2) is tied to the life of the message list (1)
+py::keep_alive<0, 1>() - Object life of the message list (1) is tied to the life of the return value (0)
+
+These flags does not cause python to take ownership of the message list
+*/
 
 template<typename T>
 void pbMessageList(py::module &m) {
-    py::class_<MessageList<T>>(m, (std::string("MessageList") + typeid(T).name()).c_str())
+    py::class_<MessageList<T>>(m, (std::string("MessageList") + type_name<T>()).c_str())
     .def(py::init<>())
     .def(py::init<const MessageList<T>&>())
     .def("setParent", &MessageList<T>::setParent)
     .def("clear", &MessageList<T>::clear)
     .def("size", &MessageList<T>::size)
-    .def("begin", &MessageList<T>::begin)
-    .def("end", &MessageList<T>::end)
-    //.def("push_back", py::overload_cast<const T&>(&MessageList<T>::push_back))
-    //.def("push_back", py::overload_cast<const T*>(&MessageList<T>::push_back))
-    .def("getSerializationSize", &MessageList<T>::getSerializationSize)
-    .def("serialize", &MessageList<T>::serialize)
-    .def("deserialize", &MessageList<T>::deserialize)
-    .def("reverseDeserialize", &MessageList<T>::reverseDeserialize)
-    //.def("toJSON", &MessageList<T>::toJSON)
+    // Object lifetime is tied to the message list
+    .def("append", py::overload_cast<const T&>(&MessageList<T>::push_back), py::keep_alive<1, 2>())
     .def("setTimeStamp", &MessageList<T>::setTimeStamp)
-    .def("setSource", &MessageList<T>::setSource)
-    .def("setSourceEntity", &MessageList<T>::setSourceEntity)
-    .def("setDestination", &MessageList<T>::setDestination)
-    .def("setDestinationEntity", &MessageList<T>::setDestinationEntity)
-    //.def(py::self = py::self)
     .def(py::self != py::self)
-    .def(py::self == py::self);
+    .def(py::self == py::self)
+    // Python operators
+    .def("__len__", &MessageList<T>::size)
+    .def("__iter__", [](const MessageList<T> &ml) { return py::make_iterator(ml.begin(), ml.end()); },
+                        py::keep_alive<0, 1>() /* Keep message list alive while iterator exists */)
+    .def("__getitem__", [](const MessageList<T> &ml, size_t i) {
+        if (i >= ml.size())
+            throw py::index_error();
+        auto it = ml.begin();
+        std::advance(it, i);  // Constant-time for random access iterators
+        return *it;
+    }, py::return_value_policy::reference_internal)
+    /// Slicing protocol 
+    .def("__getitem__", [](const MessageList<T> &ml, py::slice slice) {
+        size_t start, stop, step, slicelength;
+        if (!slice.compute(ml.size(), &start, &stop, &step, &slicelength))
+            throw py::error_already_set();
+        if(step != 1)
+            throw py::index_error("Slice indexing with step is not supported.");
+        auto begin = ml.begin(); std::advance(begin, start);
+        auto end = ml.begin(); std::advance(end, stop);
+        return py::make_iterator(begin, end);
+    }, py::keep_alive<0, 1>())
+    .def("extend", [](MessageList<T> &ml, const py::object &iterable) {
+        // Throws if not iterable
+        for(auto msg : iterable){
+            ml.push_back(msg.cast<T*>());
+        }
+    }, py::keep_alive<1, 2>());
 }
