afd07a1768c6c97fd0545b100dac60c709ca66ab
oysstu <oysstu@gmail.com>
2017-08-07 22:47:37 +0200

    Remove nonfunctioning bindings, add argument names

--- src/pbAlgorithms.cpp
+++ src/pbAlgorithms.cpp
@@ -9,8 +9,8 @@ using namespace DUNE::Algorithms;
 void pbAlgorithms(py::module &m) {
         py::module alg = m.def_submodule("algorithms", "algorithms");
         py::class_<CRC8>(alg, "CRC8")
-        .def(py::init<uint8_t, uint8_t>())
-        .def("putArray", &CRC8::putArray)
-        .def("putByte", &CRC8::putByte)
+        .def(py::init<uint8_t, uint8_t>(), py::arg("polynomial"), py::arg("value") = 0)
+        //.def("putArray", &CRC8::putArray) TODO: Change to bytes or iterable wrapper
+        .def("put_byte", &CRC8::putByte)
         .def_property("value", &CRC8::get, &CRC8::set);
 }
