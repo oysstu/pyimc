706f7289e3181c020a5c2581e259c4564f99bbce
oysstu <oysstu@gmail.com>
2017-09-27 08:28:01 +0200

    Add put_array method to CRC8 class

--- src/pbAlgorithms.cpp
+++ src/pbAlgorithms.cpp
@@ -1,16 +1,26 @@
+#include "pbUtils.hpp"
+
 #include <pybind11/pybind11.h>
 
 #include <DUNE/Algorithms/CRC8.hpp>
 
 namespace py = pybind11;
+using namespace pybind11::literals;
 
 using namespace DUNE::Algorithms;
 
 void pbAlgorithms(py::module &m) {
         py::module alg = m.def_submodule("algorithms", "algorithms");
-        py::class_<CRC8>(alg, "CRC8")
-        .def(py::init<uint8_t, uint8_t>(), py::arg("polynomial"), py::arg("value") = 0)
-        //.def("putArray", &CRC8::putArray) TODO: Change to bytes or iterable wrapper
-        .def("put_byte", &CRC8::putByte)
-        .def_property("value", &CRC8::get, &CRC8::set);
+        auto c = py::class_<CRC8>(alg, "CRC8");
+        c.def(py::init<uint8_t, uint8_t>(), py::arg("polynomial"), py::arg("value") = 0);
+        c.def("put_byte", &CRC8::putByte);
+        c.def("put_array", [](CRC8 &crc8, const py::bytes data) {
+                char* buffer;
+                ssize_t length;
+                if (PYBIND11_BYTES_AS_STRING_AND_SIZE(data.ptr(), &buffer, &length))
+                    py::pybind11_fail("Unable to extract bytes contents!");
+
+                return crc8.putArray(reinterpret_cast<uint8_t*>(buffer), static_cast<unsigned int>(length));
+        }, "data"_a, "Compute the CRC8 of an array of bytes.");
+        c.def_property("value", &CRC8::get, &CRC8::set);
 }
