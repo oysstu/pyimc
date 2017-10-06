f88028a799bbf07b9981b3f4ece4d64c4a7a65ba
oysstu <oysstu@gmail.com>
2017-10-06 17:15:59 +0200

    Add stop() function that terminates the event loop - Can be called from user (decorated) functions - Allows currently queued events to finish before terminating the event loop

--- pyimc/decorators.py
+++ pyimc/decorators.py
@@ -3,12 +3,16 @@ This file declares the decorators which provides a simple API
 for subscribing to certain messages or perform periodic tasks through the same event loop.
 """
 
-import asyncio, sys, types, socket, inspect
+import asyncio
+import sys
+import types
+import socket
+import inspect
+import logging
 from contextlib import suppress
-from typing import Dict, List, Tuple
+from typing import Dict, List, Tuple, Type, Union
 
-import pyimc
-from pyimc.udp import IMCProtocolUDP, get_imc_socket, get_multicast_socket
+from pyimc.udp import IMCProtocolUDP
 
 
 class IMCDecoratorBase:
@@ -27,7 +31,7 @@ class Periodic(IMCDecoratorBase):
     """
     Calls the decorated function every N seconds
     """
-    def __init__(self, time):
+    def __init__(self, time: Union[int, float]):
         self.time = time
 
     def __call__(self, fn, *args, **kwargs):
@@ -45,6 +49,12 @@ class Periodic(IMCDecoratorBase):
         :param fn: The function to be called
         :return: None
         """
+        # Verify function signature
+        argspec = inspect.getfullargspec(fn)
+        n_args = len(argspec.args) - 1 if 'self' in argspec.args else len(argspec.args)
+        n_required_args = n_args - (len(argspec.defaults) if argspec.defaults else 0)
+        assert n_required_args == 0, 'Functions decorated with @Periodic cannot have any required parameters.'
+
         @asyncio.coroutine
         def periodic_fn():
             while True:
@@ -79,12 +89,10 @@ class Subscribe(IMCDecoratorBase):
         # Verify function signature
         argspec = inspect.getfullargspec(fn)
         n_args = len(argspec.args) - 1 if 'self' in argspec.args else len(argspec.args)
-        if n_args < 1:
-            raise TypeError('Functions decorated with @Subscribe must have a parameter for the message.')
+        assert n_args >= 1, 'Functions decorated with @Subscribe must have a parameter for the message.'
 
         n_required_args = n_args - (len(argspec.defaults) if argspec.defaults else 0)
-        if n_required_args > 1:
-            raise TypeError('Functions decorated with @Subscribe can only have one required parameter.')
+        assert n_required_args <= 1, 'Functions decorated with @Subscribe can only have one required parameter.'
 
         # Add typing information if not already defined
         first_arg = argspec.args[1] if 'self' in argspec.args else argspec.args[0]
@@ -94,7 +102,7 @@ class Subscribe(IMCDecoratorBase):
         return fn
 
     def add_event(self, loop, instance, fn):
-        # Handled in IMCActor decorator to centralize the handling of messages
+        # Handled in IMCBase decorator to centralize the handling of messages
         pass
 
 
@@ -103,11 +111,11 @@ class IMCBase:
         self._loop = None  # type: asyncio.BaseEventLoop
         self._task_mc = None  # type: asyncio.Task
         self._task_imc = None  # type: asyncio.Task
-        self._subs = {}  # type: Dict[pyimc.Message, List[types.MethodType]]
+        self._subs = {}  # type: Dict[Exception, List[types.MethodType]]
         self._port_imc = None  # type: int
         self._port_mc = None  # type: int
 
-    def add_subscription(self):
+    def _start_subscriptions(self):
         # Add datagram endpoint for multicast announce
         multicast_listener = self._loop.create_datagram_endpoint(lambda: IMCProtocolUDP(self, is_multicast=True),
                                                                  family=socket.AF_INET)
@@ -123,14 +131,10 @@ class IMCBase:
             self._task_mc = asyncio.ensure_future(multicast_listener)
             self._task_imc = asyncio.ensure_future(imc_listener)
 
-    def setup_event_loop(self):
+    def _setup_event_loop(self):
         # Add event loop to instance
         if not self._loop:
             self._loop = asyncio.get_event_loop()
-
-        # Collect decorated member functions, add them to event loop
-        if type(self._subs) is not dict:
-            self._subs = {}
 
         decorated = [(name, method) for name, method in inspect.getmembers(self) if hasattr(method, '_decorators')]
         for name, method in decorated:
@@ -138,22 +142,45 @@ class IMCBase:
                 decorator.add_event(self._loop, self, method)
 
                 if type(decorator) is Subscribe:
-                    # Collect types of messages for each function
+                    # Collect subscribed message types for each function
                     for msg_type in decorator.subs:
                         try:
                             self._subs[msg_type].append(method)
                         except (KeyError, AttributeError):
                             self._subs[msg_type] = [method]
-        if self._subs:
-            self.add_subscription()
+
+        # Subscriptions has been collected from all decorators
+        # Add asyncio datagram endpoints to event loop
+        self._start_subscriptions()
+
+        #self._loop.set_exception_handler() TODO
+
+    def stop(self):
+        """
+        Cancels all running tasks and stops the event loop.
+        :return:
+        """
+        logging.info('Stop called by user. Cancelling all running tasks.')
+        for task in asyncio.Task.all_tasks():
+            task.cancel()
+
+        loop = self._loop
+        @asyncio.coroutine
+        def exit():
+            logging.info('Tasks cancelled. Stopping event loop.')
+            loop.stop()
+
+        asyncio.ensure_future(exit())
 
     def run(self):
+        """
+        Starts the event loop.
+        """
         # Run setup if it hasn't been done yet
         if not self._loop:
-            self.setup_event_loop()
+            self._setup_event_loop()
 
         # Start event loop
-        # TODO: set_exception_handler from @Exception(..) decorator
         try:
             self._loop.run_forever()
         except KeyboardInterrupt:
@@ -166,6 +193,7 @@ class IMCBase:
                     self._loop.run_until_complete(task)
         finally:
             self._loop.close()
+
 
 if __name__ == '__main__':
     pass
