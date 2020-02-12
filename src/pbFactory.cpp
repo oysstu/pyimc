1d35f57602f26b78f89da01c908a6f2ccff65db6
oysstu <oysstu@gmail.com>
2020-02-12 12:30:46 +0100

    DUNE/Factory: Add overload string/int for produce function

--- src/pbFactory.cpp
+++ src/pbFactory.cpp
@@ -8,7 +8,13 @@ using namespace DUNE::IMC;
 
 void pbFactory(py::module &m) {
     py::class_<Factory>(m, "Factory")
+#ifdef PYBIND11_CPP14
+    .def("produce", py::overload_cast<uint32_t>(&Factory::produce))
+    .def("produce", py::overload_cast<const std::string&>(&Factory::produce))
+#else
+    .def_static("produce", static_cast<Message* (*)(uint32_t)>(&Factory::produce))
     .def_static("produce", static_cast<Message* (*)(const std::string&)>(&Factory::produce))
+#endif
     .def_static("abbrev_from_id", &Factory::getAbbrevFromId)
     .def_static("id_from_abbrev", &Factory::getIdFromAbbrev);
 }
