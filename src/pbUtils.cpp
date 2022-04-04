d09180ac8d0dca1afffec1698db656a7403391e3
Øystein Sture <os@skarvtech.com>
2022-04-04 21:33:54 +0200

    Use string_view conversion instead of cPython function

--- src/pbUtils.cpp
+++ src/pbUtils.cpp
@@ -2,13 +2,12 @@
 #include <pybind11/pybind11.h>
 namespace py = pybind11;
 
-void bytes_to_vector(py::bytes& b, std::vector<char>& vec){
-    // The buffer is the internal storage of the bytes. Do not modify
-    char* buffer;
-    ssize_t length;
-    if (PYBIND11_BYTES_AS_STRING_AND_SIZE(b.ptr(), &buffer, &length))
-        py::pybind11_fail("Unable to extract bytes contents!");
-    vec.assign(buffer, buffer + (size_t)length * sizeof(char));
+#include <string_view>
+#include <vector>
+
+void bytes_to_vector(const py::bytes& b, std::vector<char>& vec){
+    std::string_view sv(b);
+    vec.assign(sv.begin(), sv.end());
 }
 
 py::str ascii_to_unicode_safe(const std::string &s){
--- src/pbUtils.hpp
+++ src/pbUtils.hpp
@@ -1,8 +1,11 @@
 #include <pybind11/pybind11.h>
 namespace py = pybind11;
 
+#include <vector>
+#include <string>
+
 // Assigns the bytes to the vector
-void bytes_to_vector(py::bytes& b, std::vector<char>& vec);
+void bytes_to_vector(const py::bytes& b, std::vector<char>& vec);
 
 // Decodes ASCII string while replacing non-unicode characters
 py::str ascii_to_unicode_safe(const std::string &str);
